95fc29deee37d788669e2a4eaaaf971a675c0974
Nuwan-Harshakumara-Piyarathna <nuwan.harshamatrix@gmail.com>
2022-03-07 12:10:21 +0530

    minor bug fixes

--- components/org.wso2.carbon.identity.sso.saml/src/main/java/org/wso2/carbon/identity/sso/saml/dao/SQLQueries.java
+++ components/org.wso2.carbon.identity.sso.saml/src/main/java/org/wso2/carbon/identity/sso/saml/dao/SQLQueries.java
@@ -6,16 +6,16 @@ public class SQLQueries {
     }
 
     public static class JDBCSAMLSSODAOSQLQueries {
-        public static final String ADD_SAML_APP = "INSERT INTO SAML_SSO_SP " +
-                "(ISSUER_NAME, KEY_NAME, VALUE_NAME, TENANT_ID) VALUES (?,?,?,?) ";
+        public static final String ADD_SAML_APP = "INSERT INTO IDN_SAML2_CONSUMER_APPS " +
+                "(ISSUER_NAME, PROP_KEY, PROP_VALUE, TENANT_ID) VALUES (?,?,?,?) ";
 
-        public static final String GET_SAML_APPS = "SELECT * FROM SAML_SSO_SP";
+        public static final String GET_SAML_APPS = "SELECT * FROM IDN_SAML2_CONSUMER_APPS";
 
-        public static final String GET_SAML_APP_SINGLE_ATTRIBUTE_BY_ISSUER_NAME = "SELECT * FROM SAML_SSO_SP WHERE ISSUER_NAME = ? LIMIT 1";
+        public static final String GET_SAML_APP_SINGLE_ATTRIBUTE_BY_ISSUER_NAME = "SELECT * FROM IDN_SAML2_CONSUMER_APPS WHERE ISSUER_NAME = ? LIMIT 1";
 
-        public static final String GET_SAML_APP_ALL_ATTRIBUTES_BY_ISSUER_NAME = "SELECT * FROM SAML_SSO_SP WHERE ISSUER_NAME = ?";
+        public static final String GET_SAML_APP_ALL_ATTRIBUTES_BY_ISSUER_NAME = "SELECT * FROM IDN_SAML2_CONSUMER_APPS WHERE ISSUER_NAME = ?";
 
-        public static final String REMOVE_SAML_APP_BY_ISSUER_NAME = "DELETE FROM SAML_SSO_SP " +
+        public static final String REMOVE_SAML_APP_BY_ISSUER_NAME = "DELETE FROM IDN_SAML2_CONSUMER_APPS " +
                 "WHERE ISSUER_NAME = ?";
     }
 }
